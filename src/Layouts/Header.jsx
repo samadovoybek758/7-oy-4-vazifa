8911cd33eff71b4a0696f8bcc410dc41f9cf7139
samadovoybek758 <oybek22samadov@gmail.com>
2024-10-19 20:58:09 +0500

    sds

--- src/Layouts/Header.jsx
+++ src/Layouts/Header.jsx
@@ -1,33 +1,50 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
-
-function Header({children}) {
+function Header({ children }) {
   return (
     <div>
-      <div class="navbar bg-slate-500 mb-7 px-auto mx-auto">
-  <div class="flex-1">
-    <a class="btn btn-ghost text-xl">daisyUI</a>
-  </div>
-  <div class="flex-none">
-    <ul class="menu menu-horizontal px-1">
-      <li><a>Link</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul class="bg-base-100 rounded-t-none p-2">
-            <li><a>Link 1</a></li>
-            <li><a>Link 2</a></li>
+      <div class=" bg-slate-500 mb-7 mx-auto">
+        <div className=" navbar mx-auto w-[1200px] flex justify-between">
+        <div class="flex-1">
+          <a
+            class="hidden lg:flex btn btn-primary text-3xl items-center"
+            href="/"
+          >
+            C
+          </a>
+        </div>
+        <ul className="fle flex-row gap-3">
+        <Link to={'/'}>Home</Link>
+        <Link to={'/about'}>About</Link>
+        <Link to={'/products/:id'}>Products</Link>
+        <Link to={'/cart'}>cart</Link>
+        </ul>
+        <div class="flex-none">
+          <ul class="menu menu-horizontal px-1">
+            <li>
+              <a>Link</a>
+            </li>
+            <li>
+              <details>
+                <summary>Parent</summary>
+                <ul class="bg-base-100 rounded-t-none p-2">
+                  <li>
+                    <a>Link 1</a>
+                  </li>
+                  <li>
+                    <a>Link 2</a>
+                  </li>
+                </ul>
+              </details>
+            </li>
           </ul>
-        </details>
-      </li>
-    </ul>
-  </div>
-</div>  
-{
-    children
-}
+        </div>
+        </div>
+      </div>
+      {children}
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -24,31 +24,45 @@ function Details() {
     <div className='continer1'>
         {
           product.id  && <>
-             <div className='details_cont'>
+             <div className='details_cont font-sans'>
               <img className='details_img' src={product.attributes.image} alt="" />
               <div>
-                <h1>{product.attributes.title}</h1>
-                <p>{product.attributes.company}</p>
-                <h4>{product.attributes.price}</h4>
-                <p>{product.attributes.description}</p>
+                <h1 className='text-3xl font-bold capitalize'>{product.attributes.title}</h1>
+                <h4 className='text-xl mt-2 text-gray-400 font-bold'>{product.attributes.company}</h4>
+                <p className='mt-3 text-xl'>${product.attributes.price}</p>
+                <p className='w-[455px] mt-6 text-gray-600 text-base'>{product.attributes.description}</p>
+                <p className='mt-4 text-blue-950'>colors</p>
 
-                {/* <div>
-                  {
+                <div className='flex gap-3 mt-3'>
+                  
+                 {
+                  
                     product.attributes.colors.length > 0 &&
-                    product.attributes.colors.map((colorProduct) =>{
+                    product.attributes.colors.map(Pcolor =>{
                       return (
-                        <span>
-                          style = {{
-                            bacgroundColor: colorProduct,
-                            border:
-                            color == colorProduct ? '3px solid black' : 'none',
-                          }}
-
+                        <span style = {{
+                          backgroundColor: Pcolor,
+                          border:
+                          color == Pcolor ? '3px solid black' : 'none',
+                        }}
+                        className={'block w-5 h-5 rounded-full cursor-pointer'}
+                        onClick={() =>{setColor(Pcolor)}}>
+                          
                         </span>
                       )
                     })
                   }
-                </div> */}
+                </div>
+                <select className='w-[319px] mt-3 border border-solid border-purple-800 py-3 px-3 rounded-md'>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                  <option value="6">6</option>
+                  
+                </select>
+                <button className='block mt-7 px-4 py-2 font-sans rounded-md bg-purple-700 text-gray-300 border-none'>Add TO BAG</button>
               </div>
           </div>
           </>
